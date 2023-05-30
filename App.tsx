e6fa8626a2ee7e9929f8e95a0ecedd691fc96c74
Vishnukanta <vishnukanta108@gmail.com>
2023-05-30 16:45:18 +0300

    deleting Mongo & Realm (Not Working in Expo...

--- App.tsx
+++ App.tsx
@@ -1,38 +1,58 @@
-import { useCallback } from 'react'
-import { Text, View, StyleSheet } from 'react-native'
-import { useFonts } from 'expo-font'
-import Wallet from './src/components/Wallet'
-import Realm from 'realm'
+import React, { useEffect, useState } from 'react';
+import { View, Text, TextInput, Button } from 'react-native';
+import Realm from 'realm';
 
-export default function App() {
-  const [fontsLoaded] = useFonts({
-    'Inter-Black': require('./assets/fonts/Livvic-Regular.ttf'),
-  })
-
-  const onLayoutRootView = useCallback(async () => {
-    if (fontsLoaded) {
-    }
-  }, [fontsLoaded])
-
-  if (!fontsLoaded) {
-    return null
-  }
-
-  return (
-    <View style={styles.container} onLayout={onLayoutRootView}>
-      <Text style={{ fontFamily: 'Inter-Black', fontSize: 30 }}>
-        Inter Black
-      </Text>
-      <Text style={{ fontSize: 30 }}>Platform Default</Text>
-      <Wallet />
-    </View>
-  )
+interface Task {
+  id: number;
+  title: string;
+  completed: boolean;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-})
+const App = () => {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [title, setTitle] = useState('');
+
+  useEffect(() => {
+    const realm = new Realm({ schema: [{ name: 'Task', properties: { id: 'int', title: 'string', completed: 'bool' } }] });
+    const taskList = realm.objects<Task>('Task');
+    setTasks([...taskList]);
+
+    return () => {
+      realm.close();
+    };
+  }, []);
+
+  const handleAddTask = () => {
+    const realm = new Realm({ schema: [{ name: 'Task', properties: { id: 'int', title: 'string', completed: 'bool' } }] });
+
+    realm.write(() => {
+      const newTask: Task = {
+        id: new Date().getTime(),
+        title: title,
+        completed: false,
+      };
+
+      realm.create<Task>('Task', newTask);
+    });
+
+    setTitle('');
+  };
+
+  return (
+    <View>
+      {tasks.map((task) => (
+        <Text key={task.id}>
+          {task.title} - {task.completed ? 'Completed' : 'Incomplete'}
+        </Text>
+      ))}
+      <TextInput
+        placeholder="Enter task title"
+        value={title}
+        onChangeText={(text) => setTitle(text)}
+      />
+      <Button title="Add Task" onPress={handleAddTask} />
+    </View>
+  );
+};
+
+export default App;
